067b8a665dde7435cf7a0dbc17d108e0858b4579
leandrosax10 <leandrosantosti730@gmail.com>
2021-08-24 21:41:12 -0300

    Finalização do Projeto

--- app/src/main/java/br/com/leandro/ui/BusinessCardAdapter.kt
+++ app/src/main/java/br/com/leandro/ui/BusinessCardAdapter.kt
@@ -14,7 +14,7 @@ import br.com.leandro.businesscard.databinding.ItemBusinessCardBinding
 class BusinessCardAdapter :
     ListAdapter<Businesscard, BusinessCardAdapter.ViewHolder>(DiffCallback()) {
 
-        var listenerShare: (View) -> Unit = {}
+    var listenerShare: (View) -> Unit = {}
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
